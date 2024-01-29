2947f31c6b31f6f29bc24b061b9aa64d1593ec6f
Santiago Ospina De Los Ríos <sospinar@gmail.com>
2024-01-29 17:39:51 +0000

    Remove superfluous comparisons

--- test/testhybridtreepath.cc
+++ test/testhybridtreepath.cc
@@ -89,11 +89,7 @@ int main(int argc, char** argv)
 
     // check whether comparison can be used in constexpr context
     static_assert(hybridTreePath(_1,_2,_3) == hybridTreePath(_1,_2,_3));
-    auto comp0 = hybridTreePath(_1,_2,_3) == hybridTreePath(_1,_2,_3);
-    static_assert(decltype(comp0)::value_type);
     static_assert(hybridTreePath(_1,_2,_3) != hybridTreePath(_3,_2,_1));
-    auto comp1 = hybridTreePath(_1,_2,_3) != hybridTreePath(_3,_2,_1);
-    static_assert(!decltype(comp0)::value_type);
 
     auto a = hybridTreePath(std::integral_constant<int,0>{}, std::integral_constant<int,1>{});
     auto b = hybridTreePath(std::integral_constant<std::size_t,0>{}, std::integral_constant<std::size_t,1>{});
